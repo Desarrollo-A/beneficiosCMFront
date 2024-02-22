a875bf2d3533c9215caaf74dd870962cf480a0d1
Programador36 <programador.analista36@ciudadmaderas.com>
2024-02-22 17:51:41 -0600

    Redirect de registro, cambio de alertas, y modificaciones

--- src/sections/auth/jwt/jwt-preRegister-view.jsx
+++ src/sections/auth/jwt/jwt-preRegister-view.jsx
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
+import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { useForm } from 'react-hook-form';
-import { useMemo , useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { yupResolver } from '@hookform/resolvers/yup';
 
@@ -19,33 +19,30 @@ import { useRouter } from 'src/routes/hooks';
 
 import { useBoolean } from 'src/hooks/use-boolean';
 
-import Servicios from 'src/utils/servicios';
-
 import { useAuthContext } from 'src/auth/hooks';
+import { registrarColaborador } from 'src/api/register';
 
 import Iconify from 'src/components/iconify';
-import FormProvider, {  RHFTextField } from 'src/components/hook-form';
+import { useSnackbar } from 'src/components/snackbar';
+import FormProvider, { RHFTextField } from 'src/components/hook-form';
 // ----------------------------------------------------------------------
 
 export default function PreRegisterUser({ currentUser }) {
+  const { enqueueSnackbar } = useSnackbar();
   const { login } = useAuthContext();
   const password = useBoolean();
   const passwordConfirm = useBoolean();
   const router = useRouter();
-  const  location  = useLocation();
-  const [datosEmpleado,setDatosEmpleado] = useState( location.state.data[0]);
-  const [errorMsg, setErrorMsg] = useState('');
+  const location = useLocation();
 
-
-const servicios = Servicios();
-const NewUserSchema = Yup.object().shape({
-  numEmpleado: Yup.string(),
-  name: Yup.string(),
-  newPassword: Yup.string()
+  const NewUserSchema = Yup.object().shape({
+    numEmpleado: Yup.string(),
+    name: Yup.string(),
+    newPassword: Yup.string()
       .required('Campo requerido')
       .min(6, 'La contraseña debe tener al menos 6 caracteres'),
     confirmNewPassword: Yup.string().oneOf([Yup.ref('newPassword')], 'No coicide la contraseña'),
-});
+  });
 
   const defaultValues = useMemo(
     () => ({
@@ -66,26 +63,30 @@ const NewUserSchema = Yup.object().shape({
     formState: { isSubmitting },
   } = methods;
 
-  const onSubmit = handleSubmit( (data) => {
-    const temDatos = {...datosEmpleado,password:data.confirmNewPassword}
-    setDatosEmpleado(temDatos);
-     servicios.addRegistroEmpleado(dataSb => {
-      if(dataSb.estatus === 1){
+  const onSubmit = handleSubmit(async (data) => {
+    const temDatos = { ...location.state.data[0], password: data.confirmNewPassword };
 
-        login?.(temDatos.num_empleado, temDatos.password)
-        .then(response=>{
-          if(response.result === 0){
-            setErrorMsg(response.message);
-          }else{
-              router.push(paths.dashboard.general.dash);
-          }
-        })
-      }
-    },{
-      params:temDatos
+    const registro = await registrarColaborador(temDatos);
+    if (!registro.result) {
+      enqueueSnackbar(registro.msg, {
+        variant: 'warning',
+      });
+      return false;
     }
-    );
-    console.log(errorMsg);
+    enqueueSnackbar(registro.msg, {
+      variant: 'success',
+    });
+    // Redirect con inicio de sesión
+    login?.(temDatos.num_empleado, temDatos.password).then((response) => {
+      if (response.result === 0) {
+        enqueueSnackbar(response.message, {
+          variant: 'danger',
+        });
+      } else {
+        router.push(paths.dashboard.general.dash);
+      }
+    });
+    return true;
   });
 
   const styles = {
@@ -96,65 +97,83 @@ const NewUserSchema = Yup.object().shape({
     height: '100%',
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   };
 
   return (
     <Grid container sx={styles}>
       <Grid xs={12} sm={12} md={3}>
-          <FormProvider methods={methods} onSubmit={onSubmit}>
-            <div style={{height: '50%', backgroundColor:'white', borderRadius: '25px'}}>
-              <DialogContent>
-                <Box
-                  rowGap={3}
-                  columnGap={2}
-                  display="grid"
-                  gridTemplateColumns={{
-                    xs: 'repeat(1, 1fr)',
-                    sm: 'repeat(1, 1fr)',
-                  }}
-                >
-                  <DialogTitle style={{paddingLeft:'0px'}}>Registro de usuario</DialogTitle>
-                  <RHFTextField name="numEmpleado" value={datosEmpleado.num_empleado} disabled label="Número de empleado" />
-                  <RHFTextField name="name" value={`${datosEmpleado.nombre_persona} ${datosEmpleado.pri_apellido} ${datosEmpleado.sec_apellido}`} label="Nombre completo" disabled/>
-                  <RHFTextField name="newPassword"
+        <FormProvider methods={methods} onSubmit={onSubmit}>
+          <div style={{ height: '50%', backgroundColor: 'white', borderRadius: '25px' }}>
+            <DialogContent>
+              <Box
+                rowGap={3}
+                columnGap={2}
+                display="grid"
+                gridTemplateColumns={{
+                  xs: 'repeat(1, 1fr)',
+                  sm: 'repeat(1, 1fr)',
+                }}
+              >
+                <DialogTitle style={{ paddingLeft: '0px' }}>Registro de usuario</DialogTitle>
+                <RHFTextField
+                  name="numEmpleado"
+                  value={location.state.data[0].num_empleado}
+                  disabled
+                  label="Número de empleado"
+                />
+                <RHFTextField
+                  name="name"
+                  value={`${location.state.data[0].nombre_persona} ${location.state.data[0].pri_apellido} ${location.state.data[0].sec_apellido}`}
+                  label="Nombre completo"
+                  disabled
+                />
+                <RHFTextField
+                  name="newPassword"
                   label="Contraseña"
                   type={password.value ? 'text' : 'password'}
                   InputProps={{
-                  endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton onClick={password.onToggle} edge="end">
-                      <Iconify icon={password.value ? 'solar:eye-bold' : 'solar:eye-closed-bold'} />
-                    </IconButton>
-                  </InputAdornment>
-                  
-                  ),
-                  }} />
-                  <RHFTextField name="confirmNewPassword"
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton onClick={password.onToggle} edge="end">
+                          <Iconify
+                            icon={password.value ? 'solar:eye-bold' : 'solar:eye-closed-bold'}
+                          />
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <RHFTextField
+                  name="confirmNewPassword"
                   type={passwordConfirm.value ? 'text' : 'password'}
                   label="Confirmar nueva contraseña"
                   InputProps={{
                     endAdornment: (
                       <InputAdornment position="end">
                         <IconButton onClick={passwordConfirm.onToggle} edge="end">
-                          <Iconify icon={passwordConfirm.value ? 'solar:eye-bold' : 'solar:eye-closed-bold'} />
+                          <Iconify
+                            icon={
+                              passwordConfirm.value ? 'solar:eye-bold' : 'solar:eye-closed-bold'
+                            }
+                          />
                         </IconButton>
                       </InputAdornment>
                     ),
                   }}
                 />
-                </Box>
-              </DialogContent>
+              </Box>
+            </DialogContent>
 
-              <DialogActions>
-                <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
-                  Registrarse
-                </LoadingButton>
-              </DialogActions>
-            </div>
-          </FormProvider>
-        </Grid>
+            <DialogActions>
+              <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
+                Registrarse
+              </LoadingButton>
+            </DialogActions>
+          </div>
+        </FormProvider>
       </Grid>
+    </Grid>
   );
 }
 
