3a9a59a8a30267081eb8e3fd27da7f2c0220a76a
israVL <145690244+israVL@users.noreply.github.com>
2024-02-29 17:00:59 -0600

    se ha agregado el modal con las leyendas de color sobre la citas

---
+++ src/sections/overview/calendarioespecialista/view/colors-dialog.jsx
@@ -0,0 +1,115 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { DialogTitle, DialogContent } from '@material-ui/core';
+
+import { List, Stack, Button, ListItem, Typography, DialogActions, Box } from '@mui/material';
+
+export default function ColorsDialog({ onClose }) {
+  const colors = [
+    {
+      color: '#ffa500',
+      text: 'Cita por asistir',
+    },
+    {
+      color: '#ff0000',
+      text: 'Cita cancelada',
+    },
+    {
+      color: '#808080',
+      text: 'Cita penalizada',
+    },
+    {
+      color: '#008000',
+      text: 'Cita finalizada',
+    },
+    {
+      color: '#ff4d67',
+      text: 'Cita con falta justificada',
+    },
+    {
+      color: '#00ffff',
+      text: 'Cita pendiente de pago',
+    },
+    {
+      color: '#ff0000',
+      text: 'Cita cancelada por especialista',
+    },
+    {
+      color: '#ffa500',
+      text: 'Cita que se ha reagendado',
+    },
+    {
+      color: '#ff0000',
+      text: 'Cita con pago pendiente expirado',
+    },
+    {
+      color: '#f1ff37',
+      text: 'Primera cita',
+    },
+    {
+      color: '#0000ff',
+      text: 'Cita en línea',
+    },
+  ];
+
+  return (
+    <>
+      <DialogTitle align="center">Colores de estatus </DialogTitle>
+      <DialogContent>
+        <Stack>
+          <List
+            sx={{
+              pl: 1,
+              '& .MuiListItem-root': {
+                display: 'list-item',
+              },
+            }}
+          >
+            {colors.map((color, index) => (
+              <Stack
+                direction="row"
+                alignItems="center"
+                key={index}
+                spacing={3}
+                sx={{
+                  px: { xs: 1, md: 2 },
+                  py: 1,
+                  borderRadius: '5px',
+                  margin: '5px 0',
+                }}
+              >
+                <Box
+                  sx={{
+                    minWidth: 60,
+                    maxWidth: 60,
+                    width: 60,
+                    minHeight: 30,
+                    maxHeight: 30,
+                    height: 30,
+                    borderRadius: 20,
+                    bgcolor: color.color,
+                    opacity: 0.8,
+                    '&:hover': {
+                      opacity: 0.7,
+                    },
+                  }}
+                />
+                <Typography>{color.text}</Typography>
+              </Stack>
+            ))}
+          </List>
+        </Stack>
+      </DialogContent>
+
+      <DialogActions>
+        <Button variant="contained" color="error" onClick={onClose}>
+          Cerrar
+        </Button>
+      </DialogActions>
+    </>
+  );
+}
+
+ColorsDialog.propTypes = {
+  onClose: PropTypes.func,
+};
