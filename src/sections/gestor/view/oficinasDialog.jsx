42181c38eb2bde0031264d8c7b6afa7349a308b4
israVL <145690244+israVL@users.noreply.github.com>
2024-09-03 12:13:02 -0600

    cambios en atencion por sede
    cambios respuesta a incidencias

--- src/sections/gestor/view/oficinasDialog.jsx
+++ src/sections/gestor/view/oficinasDialog.jsx
@@ -90,11 +90,11 @@ export default function OficinasDialog({ onClose, oficinasLoading, oficinas, sed
         </Stack>
       </DialogContent>
       <DialogActions>
-        {hasOffice &&
+        {/* {hasOffice && */}
             <Button variant="contained" color="error" disabled={isLoad} onClick={onClose}>
                 Cerrar
             </Button>
-        }        
+        {/* }         */}
 
         <LoadingButton variant="contained" color="success" loading={isLoad} disabled={isDisable} onClick={onSubmit}>
           Guardar
