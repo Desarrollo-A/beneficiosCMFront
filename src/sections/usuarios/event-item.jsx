52bf25329232fd234055352cf15979629db2e734
Programador36 <programador.analista36@ciudadmaderas.com>
2024-09-12 11:29:52 -0600

    Cambio de rama para revisión de catalogos

--- src/sections/usuarios/event-item.jsx
+++ src/sections/usuarios/event-item.jsx
@@ -1,31 +1,33 @@
 import PropTypes from 'prop-types';
 
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
+import Dialog from '@mui/material/Dialog';
 import MenuItem from '@mui/material/MenuItem';
 import IconButton from '@mui/material/IconButton';
+import DialogTitle from '@mui/material/DialogTitle';
 import ListItemText from '@mui/material/ListItemText';
+import DialogContent from '@mui/material/DialogContent';
 
 import { paths } from 'src/routes/paths';
 import { RouterLink } from 'src/routes/components';
 
 import { fDateTime } from 'src/utils/format-time';
-import { fCurrency } from 'src/utils/format-number';
 
 import Image from 'src/components/image';
 import Iconify from 'src/components/iconify';
+import { useBoolean } from 'src/hooks/use-boolean';
+import { Tooltip, Typography } from '@mui/material';
 import { shortDateLabel } from 'src/components/custom-date-range-picker';
 import CustomPopover, { usePopover } from 'src/components/custom-popover';
-import { Rotate90DegreesCcw } from '@mui/icons-material';
-import { Typography } from '@mui/material';
-import { margin } from '@mui/system';
+import { LoadingButton } from '@mui/lab';
 
 // ----------------------------------------------------------------------
 
 export default function EventItem({ event, onView, onEdit, onDelete }) {
   const popover = usePopover();
+  const showImage = useBoolean();
 
   const {
     idEvento,
@@ -46,26 +48,69 @@ export default function EventItem({ event, onView, onEdit, onDelete }) {
 
   const shortLabel = shortDateLabel(inicioPublicacion, finPublicacion);
 
-  const renderRating = (
+  const renderOptions = (
     <Stack
       direction="row"
       alignItems="center"
+      spacing="5px"
       sx={{
         top: 8,
         right: 8,
         zIndex: 9,
-        borderRadius: 1,
         position: 'absolute',
         p: '2px 6px 2px 4px',
-        typography: 'subtitle2',
-        bgcolor: 'warning.lighter',
+        typography: 'caption',
+        fontWeight: 'light',
       }}
     >
-      <Iconify icon="eva:star-fill" sx={{ color: 'warning.main', mr: 0.25 }} /> {50.0}
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: 100,
+          padding: '7px',
+          mr: '1px',
+          color: 'white',
+          bgcolor: 'rgba(0, 0, 0, 0.7)',
+          cursor: 'pointer',
+        }}
+      >
+        <Iconify icon="mdi:eye" sx={{ color: 'white' }} />
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: 1000,
+          padding: '7px',
+          mr: '1px',
+          color: 'white',
+          bgcolor: 'rgba(0, 0, 0, 0.7)',
+          cursor: 'pointer',
+        }}
+      >
+        <Iconify icon="mdi:edit" sx={{ color: 'white' }} />
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: 100,
+          padding: '7px 15px',
+          color: 'white',
+          bgcolor: 'rgba(0, 0, 0, 0.7)',
+          cursor: 'pointer',
+        }}
+      >
+        <Iconify icon="mdi:user" sx={{ color: 'white', mr: 0.25 }} /> 50 Confirmados
+      </Box>
     </Stack>
   );
 
-  const renderPrice = (
+  const renderDate = (
     <Stack
       direction="column"
       alignItems="center"
@@ -115,152 +160,158 @@ export default function EventItem({ event, onView, onEdit, onDelete }) {
     </Stack>
   );
 
-  const renderBlur = (
+  const renderImageButtons = (
     <Stack
       direction="row"
       alignItems="center"
+      spacing="5px"
       sx={{
-        top: 8,
-        left: 8,
+        bottom: 8,
+        right: 8,
         zIndex: 9,
-        borderRadius: 1,
-        bgcolor: 'grey.800',
         position: 'absolute',
         p: '2px 6px 2px 4px',
+        typography: 'caption',
+        fontWeight: 'light',
         color: 'common.white',
-        typography: 'subtitle2',
-        writingMode: 'vertical-lr',
-        transform: 'rotate(180deg)', // rotate(180deg),
+        // Padding
+
+        // rotar texto
+        // writingMode: 'vertical-rl',
+        // transform: 'rotate(180deg)', // rotate(180deg),
+        // Efecto blur
+        // bgcolor: 'rgba(200, 200, 200, 0.7)', // Fondo semi-transparente
+        backdropFilter: 'blur(5px)', // Desenfoque
+        WebkitBackdropFilter: 'blur(5px)', // Para Safari
+        cursor: 'pointer', // Cambia el cursor a una mano
       }}
     >
-      {/* {!!titulo && (
-        <Box component="span" sx={{ color: 'grey.500', mr: 0.25, textDecoration: 'line-through' }}>
-          {fCurrency(50)}
-        </Box>
-      )}
-      {fCurrency(51)} */}
+      <LoadingButton
+        component="span"
+        sx={{
+          color: 'white',
+          typography: 'subtitle1',
+          fontWeight: 'light',
+          mr: 0.25,
+          bgcolor: 'rgba(200, 200, 200, 0.4)',
+        }}
+        onClick={() => alert('Consultando ubicación...')}
+      >
+        <Iconify width={17} icon="bxs:map" />
+        Consulta la ubicación
+      </LoadingButton>
+      <LoadingButton
+        component="span"
+        sx={{
+          color: 'white',
+          typography: 'subtitle1',
+          fontWeight: 'light',
+          mr: 0.25,
+          bgcolor: 'rgba(200, 200, 200, 0.4)',
+        }}
+        onClick={() => alert('Confirmando evento...')}
+      >
+        Confirmar asistencia
+      </LoadingButton>
     </Stack>
   );
 
-  const renderImages = (
-    <Stack
-      spacing={0.5}
-      direction="row"
-      sx={{
-        p: (theme) => theme.spacing(1, 1, 0, 1),
-      }}
-    >
+  const renderCover = (
+    <Stack spacing={0.5} direction="row">
       <Stack flexGrow={1} sx={{ position: 'relative' }}>
-        {renderPrice}
-        {renderRating}
+        {renderDate}
+        {renderOptions}
         <Image
           alt={imagen}
           src={imagen}
-          sx={{ margin: '0px 0px 0px 35px', borderRadius: 1, height: 300, width: 1 }}
+          sx={{ margin: '0px 0px 0px 35px', borderRadius: 1, height: 250, width: 1 }}
         />
       </Stack>
     </Stack>
   );
 
-  const renderTexts = (
-    <ListItemText
-      sx={{
-        p: (theme) => theme.spacing(2.5, 2.5, 2, 2.5),
-      }}
-      primary={`Posted date: ${fDateTime(fechaEvento)}`}
-      secondary={
-        <Link component={RouterLink} href={paths.dashboard.tour.details(idEvento)} color="inherit">
-          {titulo}
-        </Link>
-      }
-      primaryTypographyProps={{
-        typography: 'caption',
-        color: 'text.disabled',
-      }}
-      secondaryTypographyProps={{
-        mt: 1,
-        noWrap: true,
-        component: 'span',
-        color: 'text.primary',
-        typography: 'subtitle1',
-      }}
-    />
-  );
-
-  const renderInfo = (
-    <Stack
-      spacing={1.5}
-      sx={{
-        position: 'relative',
-        p: (theme) => theme.spacing(0, 2.5, 2.5, 2.5),
-      }}
-    >
-      <IconButton onClick={popover.onOpen} sx={{ position: 'absolute', bottom: 20, right: 8 }}>
-        <Iconify icon="eva:more-vertical-fill" />
-      </IconButton>
-
-      {[
-        {
-          label: ubicacion,
-          icon: <Iconify icon="mingcute:location-fill" sx={{ color: 'error.main' }} />,
-        },
-        {
-          label: shortLabel,
-          icon: <Iconify icon="solar:clock-circle-bold" sx={{ color: 'info.main' }} />,
-        },
-        {
-          label: `50 Booked`,
-          icon: <Iconify icon="solar:users-group-rounded-bold" sx={{ color: 'primary.main' }} />,
-        },
-      ].map((item) => (
-        <Stack
-          key={item.label}
-          spacing={1}
-          direction="row"
-          alignItems="center"
-          sx={{ typography: 'body2' }}
-        >
-          {item.icon}
-          {item.label}
-        </Stack>
-      ))}
-    </Stack>
-  );
-
   const renderInfoEvent = (
-    <ListItemText
-      sx={{
-        p: (theme) => theme.spacing(2.5, 2.5, 2, 2.5),
-      }}
-      primary={`Posted date: ${fDateTime(fechaEvento)}`}
-      secondary={
-        <Link component={RouterLink} href={paths.dashboard.tour.details(idEvento)} color="inherit">
-          {titulo}
-        </Link>
-      }
-      primaryTypographyProps={{
-        typography: 'caption',
-        color: 'text.disabled',
-      }}
-      secondaryTypographyProps={{
-        mt: 1,
-        noWrap: true,
-        component: 'span',
-        color: 'text.primary',
-        typography: 'subtitle1',
-      }}
-    />
+    <Stack direction="row" sx={{ flex: 1 }}>
+      <Box sx={{ width: '70%', margin: '0px 0px 0px 35px', padding: '1px' }}>
+        <Box
+          sx={{
+            flex: 1,
+            typography: 'caption',
+            fontWeight: 'light',
+            fontSize: 10,
+            color: '#757575',
+            whiteSpace: 'nowrap', // Evita que el texto se divida en varias líneas
+            overflow: 'hidden', // Oculta el texto que excede el ancho del contenedor
+            textOverflow: 'ellipsis',
+          }}
+        >
+          <Tooltip title="Creado el 13 Octubre 2024, 10:35am">
+            Creado el 13 Octubre 2024, 10:35am
+          </Tooltip>
+        </Box>
+        <Box
+          sx={{
+            flex: 1,
+            typography: 'subtitle1',
+            fontWeight: 'bold',
+            fontSize: 16,
+            color: '#00263A',
+            whiteSpace: 'nowrap', // Evita que el texto se divida en varias líneas
+            overflow: 'hidden', // Oculta el texto que excede el ancho del contenedor
+            textOverflow: 'ellipsis',
+          }}
+        >
+          <Tooltip title="Celebración de día de muertos">Celebración de día de muertos</Tooltip>
+        </Box>
+        <Stack direction="row" sx={{ flex: 1 }}>
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mr: '2px' }}>
+            <Iconify width={17} icon="bxs:map" />
+          </Box>
+          <Box
+            sx={{
+              typography: 'caption',
+              fontWeight: 'light',
+              fontSize: 10,
+              whiteSpace: 'nowrap', // Evita que el texto se divida en varias líneas
+              overflow: 'hidden', // Oculta el texto que excede el ancho del contenedor
+              textOverflow: 'ellipsis',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Tooltip title="Camino a Eco-Centro s/n, 76245 Santiago de Querétaro, Querétaro, MEXICO">
+              Camino a Eco-Centro s/n, 76245 Santiago de Querétaro, Querétaro, MEXICO
+            </Tooltip>
+          </Box>
+        </Stack>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          backgroundColor: '#00263A',
+          width: '30%',
+          color: 'white',
+          justifyContent: 'center',
+          alignItems: 'center',
+          cursor: 'pointer', // Cambia el cursor a una mano
+          '&:hover': {
+            backgroundColor: '#004466', // Cambia el color al pasar el mouse
+          },
+          typography: 'subtitle2',
+          fontWeight: 'light',
+        }}
+        onClick={() => showImage.onTrue()}
+      >
+        MÁS
+      </Box>
+    </Stack>
   );
 
   return (
     <>
-      <Card>
-        {renderImages}
-
+      <Card sx={{ backgroundColor: '#F0ECE0', padding: '0px' }}>
+        {renderCover}
         {renderInfoEvent}
-        {/* {renderTexts} */}
-
-        {/* {renderInfo} */}
       </Card>
 
       <CustomPopover
@@ -300,6 +351,40 @@ export default function EventItem({ event, onView, onEdit, onDelete }) {
           Delete
         </MenuItem>
       </CustomPopover>
+
+      <Dialog open={showImage.value} onClose={showImage.onFalse}>
+        <DialogContent
+          sx={{
+            margin: '0px',
+            padding: '0px',
+            backgroundImage: `url(${imagen})`,
+            height: '800px',
+            width: '1200px',
+            backgroundSize: 'fit',
+            backdropFilter: 'blur(800px)',
+          }}
+        >
+          {renderImageButtons}
+          {/* <Image
+            alt={imagen}
+            src={imagen}
+            sx={{ margin: '0px', borderRadius: 1, height: '100%', width: 1 }}
+          /> */}
+          {/* Pseudo-elemento para aplicar el blur en el 20% inferior */}
+          <Box
+            sx={{
+              position: 'absolute',
+              bottom: 0,
+              left: 0,
+              width: '100%',
+              height: '25%', // Aplica el blur al 20% inferior
+              background:
+                'linear-gradient(to top, rgba(0, 0, 0, 1) 10%, rgba(0, 0, 0, 0.9) 20%, rgba(0, 0, 0, 0.8) 30%, rgba(0, 0, 0, 0.7) 40%, rgba(0, 0, 0, 0.6) 50%, rgba(0, 0, 0, 0.5) 60%, rgba(0, 0, 0, 0.4) 70%, rgba(0, 0, 0, 0.3) 80%, rgba(0, 0, 0, 0.15) 90%, rgba(0, 0, 0, 0.0) 100%)', // Gradiente de blanco a transparente
+              // backdropFilter: 'blur(8px)', // Efecto de blur
+            }}
+          />
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
